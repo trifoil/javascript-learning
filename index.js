02453446fb96a2fcd4252c3d06adaaec229abcd0
trifoil <unixsslayer@gmail.com>
2024-09-08 01:50:39 +0200

    a

--- index.js
+++ index.js
@@ -7,4 +7,23 @@ window.alert('!!I like pizza!!');
 */
 
 document.getElementById("myH1").textContent = `Holy shi`
-document.getElementById("myP").textContent = `dang`
+
+let x;
+x = 100;
+console.log(x)
+
+let age = 24 ** 2;
+age = age +1;
+let price = 10.99;
+let gpa = 2.1;
+let firstname = "bob";
+let online = true
+
+
+console.log(`you are ${age} years old`);
+console.log(`the price is ${price}`);
+console.log(`Your gpa is : ${gpa}`)
+
+console.log( typeof firstname, typeof age, typeof price, typeof online)
+
+document.getElementById("myP").textContent = `you are ${age} years old`
